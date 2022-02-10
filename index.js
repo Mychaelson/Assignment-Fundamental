edb39eb3d535c64a6f9d3de1e830081d1facf633
Mychaelson <sonmychael@gmail.com>
2022-02-10 14:55:23 +0700

    feat: added .indexOf() duplicate fuction

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+const buah = ["apel", "mangga", "jeruk", "semangka", "jambu", "melon"];
+
+// -----------------------------------------------------
+
 // duplikasi method .indexOf()
 
 // mencari index dari sebuah item di array
 // akan return sebuah number (index)
+
+const indexOf = (arr, input) => {
+  for (let i = 0; i < buah.length; i++) {
+    if (buah[i] == input) {
+      return i;
+    }
+  }
+};
+
+console.log(indexOf(buah, "melon"));
 
 // ------------------------------------------------------
 
