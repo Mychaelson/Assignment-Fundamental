cefc05a6a8416969db61a555bf76499821d477c2
Mychaelson <sonmychael@gmail.com>
2022-02-10 14:46:26 +0700

    docs: added instruction

---
+++ index.js
@@ -0,0 +1,15 @@
+// duplikasi method .indexOf()
+
+// mencari index dari sebuah item di array
+// akan return sebuah number (index)
+
+// ------------------------------------------------------
+
+// duplikasi method .find()
+
+// Conditions (find)
+// 1. Mencari sebuah value (item) dalam array yang
+//    memenuhi condition yang kita berikan
+// 2. Callback function kita harus return boolean
+// 3. Ketika callback function return true, loop akan berhenti
+//    lalu function find akan return item dari array.
